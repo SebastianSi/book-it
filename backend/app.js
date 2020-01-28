bc11cd47088db626a3752970bf186cf78c53dd43
sebastian.sirb <sebastian.sirb90@gmail.com>
2020-01-28 19:13:00 +0200

    renamed routes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import setAppRoutes from './controllers/api';
+import setAppRoutes from './controllers/routes';
 import models, { connectDb, closeDbConnection } from './models';
 
 // Set up the express app
